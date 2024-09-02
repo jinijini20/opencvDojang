4090d53e7675c91a06eaea546ebfca6d945ed1bb
jinijini20 <jimini301@naver.com>
2024-09-02 16:39:29 +0900

    video_record 완성

---
+++ webcamEx.py
@@ -0,0 +1,14 @@
+import cv2,sys
+
+#videoCapture() 클래스 객체 생성
+
+cap=cv2.VideoCapture(0)
+
+print(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),\
+      int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+
+cap.set(cap.get(cv2.CAP_PROP_FRAME_WIDTH,640)
+cap.set(cap.get(cv2.CAP_))
+if cap.isOpened():
+    cap.release()
